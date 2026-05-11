9a0c1559ffe80231c839e2da4d93c7ef887fe1f9
Jumpifequal <zapped.bengrimm@gmail.com>
2026-05-11 23:03:17 +0200

    Add files via upload

--- build.bat
+++ build.bat
@@ -4,13 +4,22 @@ setlocal
 set SRC=AIProcess-Terminate.cpp
 set OUT=AIProcess-Terminate.exe
 set OBJ_DIR=build_obj
+set RC_SRC=resource.rc
+set RC_OBJ=%OBJ_DIR%\resource.res
 
 if not exist "%OBJ_DIR%" mkdir "%OBJ_DIR%"
+
+rc /nologo /fo "%RC_OBJ%" "%RC_SRC%"
+if %ERRORLEVEL% neq 0 (
+    echo.
+    echo [FAILED] Resource compiler error.
+    exit /b %ERRORLEVEL%
+)
 
 cl /W4 /WX /std:c++17 /EHsc /O2 /D_UNICODE /DUNICODE ^
    /Fo"%OBJ_DIR%\\" /Fe"%OUT%" ^
    %SRC% ^
-   /link psapi.lib comctl32.lib user32.lib /SUBSYSTEM:WINDOWS
+   /link psapi.lib comctl32.lib user32.lib /SUBSYSTEM:WINDOWS "%RC_OBJ%"
 
 if %ERRORLEVEL% neq 0 (
     echo.
